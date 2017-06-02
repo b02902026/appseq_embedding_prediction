74c482d9ca201bdb33a410a873e9804a6251c300
ben chen <b02902026@ntu.edu.tw>
2017-06-02 16:51:18 +0800

    new spider

--- src/playSpider/googleplay/spiders/googleplaySpider.py
+++ src/playSpider/googleplay/spiders/googleplaySpider.py
@@ -15,9 +15,30 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self,response):
 
+        for url in response.xpath('//a[@class="see-more play-button small id-track-click apps id-responsive-see-more"]'):
+            print("len=",len(url.extract()))
+            #c_name = url.xpath('a[@class="title-link id-track-click"]/text()')[0].extract()
+            #print("-------------"+c_name+"-----------------")
+            target = "https://play.google.com"+url.xpath('@href')[0].extract()
+            c_name = "popular"
+            yield  scrapy.Request(
+                target + "?hl=en",
+                meta ={"cat":c_name},
+                callback = self.parse_data
+            )
+            yield  scrapy.Request(
+                target + "&hl=en",
+                meta ={"cat":c_name},
+                callback = self.parse_data
+            )
+
+        return
+
         for url in response.xpath('//div[@class="dropdown-submenu"]/ul/li[@class="action-dropdown-outer-list-item list-item"]'):
             for cat in url.xpath('ul/li[@class="child-submenu-link-wrapper"]/a[@class="child-submenu-link"]'):
                 cat_name = cat.xpath('text()')[0].extract()
+                with open('cat_log','a+') as f:
+                    f.write("category name:"+cat_name+'\n')
                 cat_url = "https://play.google.com" + cat.xpath('@href')[0].extract()
 
                 yield scrapy.Request(cat_url+"?hl=en",meta={'cat':cat_name}, callback=self.parse_cat)
@@ -25,14 +46,21 @@ class QuotesSpider(scrapy.Spider):
     def parse_cat(self, response):
 
             for url in response.xpath('//a[@class="see-more play-button small id-track-click apps id-responsive-see-more"]'):
-                target = "https://play.google.com"+url.xpath("@href")[0].extract()+"?hl=en"
+                target = "https://play.google.com"+url.xpath("@href")[0].extract()
                 yield  scrapy.Request(
-                    target,
+                    target + "?hl=en",
+                    meta ={"cat":response.meta["cat"]},
+                    callback = self.parse_data
+                )
+                yield  scrapy.Request(
+                    target + "&hl=en",
                     meta ={"cat":response.meta["cat"]},
                     callback = self.parse_data
                 )
 
     def parse_data(self, response):
+        if response.status == 404:
+            return
 
         for app_obj in response.xpath('//div[@class="card no-rationale square-cover apps small"]/div[@class="card-content id-track-click id-track-impression"]'):
             link = app_obj.xpath('div[@class="details"]/a[@class="title"]/@href')[0].extract()
@@ -49,7 +77,7 @@ class QuotesSpider(scrapy.Spider):
 
         more = response.xpath('//a[@class="title-link id-track-click"]/@href')[0].extract()
         #print "MORE:",more
-        description = response.xpath('//div[@class="details"]/div[@class="description"]/text()')[0].extract()
+        description = response.xpath('//div[@class="show-more-content text-body"]/div[@jsname="C4s9Ed"]/text()')[0].extract()
         description = "".join(description)
         name = response.xpath('//div[@class="id-app-title"]/text()')[0].extract().strip()
         yield scrapy.Request(
